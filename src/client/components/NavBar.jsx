35533975cb7dc7edf5f49931f42b2ac3a05580f2
adrienne365 <adrienne365@gmail.com>
2023-11-01 11:33:37 -0400

    pre signup

--- src/client/components/NavBar.jsx
+++ src/client/components/NavBar.jsx
@@ -1,46 +1,37 @@
+import { Link } from "react-router-dom";
+import { useState, useEffect } from 'react';
+
 const NavBar = () => {
-    // const [loggedIn, setLoggedIn] = useState(false)
+  const [signedIn, setSignedIn] = useState(false)
+  
+  useEffect(() => {  
+    async function renderNavBar() {
+      const token = sessionStorage.getItem("token")
+      if (token) { 
+        console.log("signed in")
+        setSignedIn(true);
+  }}
+  renderNavBar()
+    },[signedIn]);
+
+  return (
+    // <div>
+      <nav>
+        <Link to="/" className="nav-item">
+          Home
+        </Link>
+      {/* </nav> */}
     
-    // useEffect(() => {  
-    //   async function renderNavbar() {
-    //     const token = sessionStorage.getItem("token")
-    //     if (token) { 
-    //       console.log("logged in")
-    //       setLoggedIn(true);
-    // }}
-    // renderNavbar()
-    //   },[loggedIn]);
-  
-    return (
-      // <div>
-        <nav>
-          <Link to="/" className="nav-item">
-            Home
-          </Link>
-        {/* </nav> */}
-      
-        {/* {loggedIn ? (
-          <nav> */}
-          <Link to="/profile" className="nav-item">
-          Profile
-          </Link>
-            <button className="button" onClick={Logout}>Logout</button>
-          {/* </nav>) :  */}
-          
-          {/* (<nav> */}
-          <Link to="/login" className="nav-item">
-          Login
+      {/* {loggedIn ? (
+        <nav> */}
+        <Link to="/profile" className="nav-item">
+        Profile
         </Link>
-        <Link to="/register" className="nav-item">
-          Register
-        </Link>
-        <Link to="/create" className="nav-item">
-          Shop Wines
-        </Link>
-      </nav>
-    );
-  };
-  
-  
-  export default NavBar;
-  
+          <button className="button" onClick={Logout}>Logout</button>
+        </nav>)
+        };
+        
+        
+
+
+export default NavBar;
