6f1187af5361a26be596be98444ced675211e008
adrienne365 <adrienne365@gmail.com>
2023-11-02 11:35:24 -0400

    create/delete/detailed/edit *not tested*

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,6 +1,13 @@
 import { useState } from 'react';
+import { Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import NavBar from './components/NavBar';
-import Login from './components/Login';
+import SignUp from './components/SignUp';
+import SignIn from './componenets/SignIn';
+import CreateWine from './components/CreateWine';
+import EditWine from './components/EditWine';
+import DetailedWine from './components/DetailedWine';
+
 
 function App() {
   const [token, setToken] = useState(null);
