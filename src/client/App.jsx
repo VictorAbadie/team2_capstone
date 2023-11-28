a47eddd91bd8519b82730cad17c4069b11af611f
sawyerno <sawyernoland2005@gmail.com>
2023-11-27 20:52:02 -0500

    bug fixes

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -12,12 +12,12 @@ import SignUpForm from './components/SignUpForm';
 import CreateWine from './components/CreateWine';
 import EditWine from './components/EditWine';
 import DeleteWine from './components/DeleteWine';
-// import DetailedWine from './components/DetailedWine';
+import DetailedWine from './components/DetailedWine';
 import SetAdminFunction from './components/isAdmin';
 
 
 function App() {
-  // const [token, setToken] = useState(null);
+  const [token, setToken] = useState(null);
   const [cart, setCart] = useState([])
 
   useEffect(() => {
@@ -26,7 +26,7 @@ function App() {
 
 
   const [isAdmin, setIsAdmin] = useState(false) 
-  const [token, setToken] = useState(null)
+  // const [token, setToken] = useState(null)
   console.log(isAdmin);
   const storageToken = localStorage.getItem("token");
   useEffect(() => {
@@ -56,15 +56,14 @@ function App() {
           {/* <Route path="/profile" element={<Profile token={token}/>} /> */}
 
           {/* Protected Routes */}
-          {/* <Route path="/:id" element={<DetailedWine />} /> */}
-          <Route path="/EditWine" element={<EditWine />} />
+          <Route path="/:id" element={<DetailedWine />} />
+          {/* <Route path="/EditWine" element={<EditWine />} /> */}
           <Route path="/success" element={<Success/>} /> 
           <Route path="/cancel" element={<Cancel/>} /> 
 
           <Route path='/admin' element={<SetAdminFunction isAdmin={isAdmin} setIsAdmin={setIsAdmin}/>} />
             <Route path="/CreateWine" element={<CreateWine isAdmin={isAdmin} setIsAdmin={setIsAdmin}/>} />
           <Route path="/DeleteWine" element={<DeleteWine isAdmin={isAdmin} setIsAdmin={setIsAdmin}/>} />
-          {/* <Route path="/DetailedWine" element={<DetailedWine />} /> */}
           <Route path="/EditWine" element={<EditWine isAdmin={isAdmin} setIsAdmin={setIsAdmin}/>} />
         </Routes>
       </BrowserRouter>
